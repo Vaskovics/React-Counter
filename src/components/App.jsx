07803ccb83dfac338e77d228c0f0a5037411c8e4
Vaskovics <78701608+Vaskovics@users.noreply.github.com>
2022-07-04 17:07:16 +0100

    Complected Counter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,16 +11,26 @@ function App() {
   const redColor = { color: "red" };
   const greenColor = { color: "green" };
   const whiteColor = { color: "white" };
-
+  const blueColor = { color: "blue" };
   return (
     <div className="container">
-      {count > 5 && <h2>You Won!</h2>}
-      <h1 style={count < 0 ? redColor : count === 0 ? whiteColor : greenColor}>
+      <h1
+        style={
+          count < 0
+            ? redColor
+            : count === 0
+            ? whiteColor
+            : count > 5
+            ? blueColor
+            : greenColor
+        }
+      >
         {count}
       </h1>
       <button onClick={increase}>+</button>
       <button onClick={decrease}>-</button>
       {count < 0 && <h2>You Lost!</h2>}
+      {count > 5 && <h2>You Won!</h2>}
     </div>
   );
 }
